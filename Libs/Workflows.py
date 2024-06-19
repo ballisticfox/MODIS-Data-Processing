55675210e78689ed0c75a91c105df3297b44de47
ballisticfox <spotthedot2345@gmail.com>
2024-06-18 22:29:55 -0500

    SIN Workflow

--- Libs/Workflows.py
+++ Libs/Workflows.py
@@ -132,3 +132,74 @@ class Workflows:
             os.remove(f"{dir_path}{DataSet}.{AquisitionDate}_Mask.tif")
             
             os.remove(f"{dir_path}{DataSet}.{AquisitionDate}_RGB.tif")
+        
+    @staticmethod    
+    def SIN_Workflow(hdfFile: str, removeProcessFiles: bool) -> None:
+        dir_path = Util.GetDataDirectory()
+        print(f"Processing: {hdfFile}")
+        hdfData = hdfFile.split(".")
+        print(hdfData)
+        DataSet = hdfData[0]
+        AquisitionDate = hdfData[1]
+        GridCoord = hdfData[2]
+        ProductVersion = hdfData[3]
+        ProductionDate = hdfData[4]
+        fileExtension = hdfData[5]
+        
+        
+        HEG.GenerateGrid(f"{DataSet}.{AquisitionDate}.{GridCoord}.Band1", f"{DataSet}.{AquisitionDate}.{GridCoord}.{ProductVersion}.{ProductionDate}", "MODIS_Grid_500m_2D", "sur_refl_b01_1", 1, f"{DataSet}.{GridCoord}.{AquisitionDate}_Band1")
+        HEG.GenerateGrid(f"{DataSet}.{AquisitionDate}.{GridCoord}.Band3", f"{DataSet}.{AquisitionDate}.{GridCoord}.{ProductVersion}.{ProductionDate}", "MODIS_Grid_500m_2D", "sur_refl_b03_1", 1, f"{DataSet}.{GridCoord}.{AquisitionDate}_Band3")
+        HEG.GenerateGrid(f"{DataSet}.{AquisitionDate}.{GridCoord}.Band4", f"{DataSet}.{AquisitionDate}.{GridCoord}.{ProductVersion}.{ProductionDate}", "MODIS_Grid_500m_2D", "sur_refl_b04_1", 1, f"{DataSet}.{GridCoord}.{AquisitionDate}_Band4")
+        HEG.GenerateGrid(f"{DataSet}.{AquisitionDate}.{GridCoord}.StateQA", f"{DataSet}.{AquisitionDate}.{GridCoord}.{ProductVersion}.{ProductionDate}", "MODIS_Grid_1km_2D", "state_1km_1", 1, f"{DataSet}.{GridCoord}.{AquisitionDate}_StateQA")
+
+        # Level 1
+        # HEG.GenerateGeoTiff(f"{DataSet}.{AquisitionDate}.Band1")
+        t1 = threading.Thread(target=HEG.GenerateGeoTiff, args=(f"{DataSet}.{AquisitionDate}.{GridCoord}.Band1",))
+        # HEG.GenerateGeoTiff(f"{DataSet}.{AquisitionDate}.Band3")
+        t2 = threading.Thread(target=HEG.GenerateGeoTiff, args=(f"{DataSet}.{AquisitionDate}.{GridCoord}.Band3",))
+        # HEG.GenerateGeoTiff(f"{DataSet}.{AquisitionDate}.Band4")
+        t3 = threading.Thread(target=HEG.GenerateGeoTiff, args=(f"{DataSet}.{AquisitionDate}.{GridCoord}.Band4",))
+        
+        t4 = threading.Thread(target=HEG.GenerateGeoTiff, args=(f"{DataSet}.{AquisitionDate}.{GridCoord}.StateQA",))
+
+        
+        t1.start()
+        t2.start()
+        t3.start()
+        t4.start()
+        
+        t1.join()
+        t2.join()
+        t3.join()
+        t4.join()
+        
+        t6 = threading.Thread(target=Processes.INTtoUNIT, args=(f"{DataSet}.{GridCoord}.{AquisitionDate}_Band1", 2, 2.2, f"{DataSet}.{GridCoord}.{AquisitionDate}_Band1-Corrected",))
+        t7 = threading.Thread(target=Processes.INTtoUNIT, args=(f"{DataSet}.{GridCoord}.{AquisitionDate}_Band3", 2, 2.2, f"{DataSet}.{GridCoord}.{AquisitionDate}_Band3-Corrected",))
+        t8 = threading.Thread(target=Processes.INTtoUNIT, args=(f"{DataSet}.{GridCoord}.{AquisitionDate}_Band4", 2, 2.2, f"{DataSet}.{GridCoord}.{AquisitionDate}_Band4-Corrected",))
+        
+        t6.start()
+        t7.start()
+        t8.start()
+        
+        t6.join()
+        t7.join()
+        t8.join()
+        
+        print("Composing Color")
+        Processes.RGBCompose(f"{DataSet}.{GridCoord}.{AquisitionDate}_Band1-Corrected", f"{DataSet}.{GridCoord}.{AquisitionDate}_Band4-Corrected", f"{DataSet}.{GridCoord}.{AquisitionDate}_Band3-Corrected", f"{DataSet}.{GridCoord}.{AquisitionDate}_RGB")
+        
+        # Level 3
+        
+        # print("Generating Data Mask")
+        # Processes.GenerateDataMask(f"{DataSet}.{AquisitionDate}_StateQA", f"{DataSet}.{AquisitionDate}_DataMask")
+        t9 = threading.Thread(target=Processes.GenerateDataMask, args=(f"{DataSet}.{GridCoord}.{AquisitionDate}_StateQA", f"{DataSet}.{GridCoord}.{AquisitionDate}_DataMask",))
+        
+        # print("Generating No Data Mask")
+        # Processes.GenerateNoDataMask(f"{DataSet}.{AquisitionDate}_RGB", f"{DataSet}.{AquisitionDate}_NoData")
+        t10 = threading.Thread(target=Processes.GenerateNoDataMask, args=(f"{DataSet}.{GridCoord}.{AquisitionDate}_RGB", f"{DataSet}.{GridCoord}.{AquisitionDate}_NoData",))
+        
+        t9.start()
+        t10.start()
+        
+        t9.join()
+        t10.join()
--- Program.py
+++ Program.py
@@ -6,38 +6,12 @@ import wand.image
 from wand.image import Image
 
 def Main():
-    dir_path = Util.GetDataDirectory()
-    dir_list = os.listdir(dir_path)
-    
-    for file in dir_list:
-        print(file)
-        if ".hdf" in file:
-            print(file)
-            Workflows.CMG_Workflow(file, True)
-        
-    # operator = "over"
-          
-    # with Image(filename=dir_path+dir_list[0]) as data:
-    #     with Image(filename=dir_path+dir_list[1]) as img:
-    #         data.composite(img, operator = "over")
-    #     data.save(filename=dir_path+"composite.tif")
-        
-    # dir_list.pop(0)
-    # dir_list.pop(0)
-    
-    # for file in dir_list:
-    #     with Image(filename=dir_path+"composite.tif") as data:
-    #         with Image(filename=dir_path+file) as img:
-    #             data.composite(img, operator = "over")
-    #         data.save(filename=dir_path+"composite.tif")
-    
-    
-    # for file in dir_list:
-    #     print(file)
-    #     Workflows.CMG_Workflow(file, True)
+    files = [
+        "MOD09GA.A2023245.h20v04.061.2023247025655.hdf",
 
-    
-        
+    ]
+    for file in files:
+        Workflows.SIN_Workflow(file, False)
     
     
 Main()
