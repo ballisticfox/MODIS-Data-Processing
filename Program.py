d05466459686b54933d65882464a6d30c8f895dd
ballisticfox <spotthedot2345@gmail.com>
2024-06-20 00:50:46 -0500

    Proper Workflow setup

--- Program.py
+++ Program.py
@@ -3,11 +3,17 @@ from Libs.Processes import *
 from Libs.Util import *
 from Libs.Networking import *
 from Libs.Tile import *
+from Libs.Token import *
 
 def Main():
-    tileList = Networking.ProcessCSV("LAADS_query.2024-06-20T05_11.csv")
+    tileList = Networking.ProcessCSV("LAADS_query.2024-06-20T05_40.csv")
     for tile in tileList:
         print(tile.productName)
+        Networking.downloadURL(tile, Token.GetToken())
+    
+    for index in range(0,int(len(tileList)/2)):
+        print(tileList[index].fileName)
+        Workflows.MOD09("MYD09", tileList[index].fileName, True)
     
 Main()
 print("Process Complete")
